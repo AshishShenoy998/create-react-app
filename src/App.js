fee0e66adb7c2a5a3e4c0e971d7ba755d169edda
Ashish Shenoy <ashishshenoy998@gmail.com>
2021-07-25 00:10:43 +0530

    third change

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-          Deploymet Is Being Done!!!!!!!!!!
+          Deployed
       </header>
     </div>
   );
